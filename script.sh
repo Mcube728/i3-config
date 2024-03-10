94005575197ca4944f4535717dd9556f520d7d1c
Mcube728 <boredAstronaut728@protonmail.com>
2024-03-10 12:19:08 +0530

    updated script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,14 @@
-wget -0 https://raw.githubusercontent.com/Mcube728/i3-config/main/i3config
+mkdir ~/.config/i3
+mkdir ~/.config/i3status
+
+wget -0 ~/config/i3/config https://raw.githubusercontent.com/Mcube728/i3-config/main/i3/config
+
+wget -0 ~/config/i3status/calendar.sh https://raw.githubusercontent.com/Mcube728/i3-config/main/i3status/calendar.sh
+wget -0 ~/config/i3status/click_time.sh https://raw.githubusercontent.com/Mcube728/i3-config/main/i3status/click_time.sh
+wget -0 ~/config/i3status/cpu.sh https://raw.githubusercontent.com/Mcube728/i3-config/main/i3status/cpu.py
+wget -0 ~/config/i3status/disk.py https://raw.githubusercontent.com/Mcube728/i3-config/main/i3status/disk.py
+wget -0 ~/config/i3status/memory.py https://raw.githubusercontent.com/Mcube728/i3-config/main/i3status/memory.py
+wget -0 ~/config/i3status/mybar.sh https://raw.githubusercontent.com/Mcube728/i3-config/main/i3status/mybar.sh
+wget -0 ~/config/i3status/config https://raw.githubusercontent.com/Mcube728/i3-config/main/i3status/config
+
+chmod +x mybar.sh calendar.sh click_time.sh
